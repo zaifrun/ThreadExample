fa8b133d7acec1ac838ac018e899b9a65d883a93
zaifrun <martinknud@gmail.com>
2016-04-11 12:51:33 +0200

    added comments.

--- app/src/main/java/com/example/threadexample/ThreadExampleActivity.java
+++ app/src/main/java/com/example/threadexample/ThreadExampleActivity.java
@@ -1,26 +1,31 @@
 package com.example.threadexample;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.Menu;
 import android.view.View;
 import android.widget.TextView;
-import android.os.Handler;
-import android.os.Message;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ThreadExampleActivity extends Activity {
 
-	Handler handler = new Handler() {
-		  @Override
+	static TextView myTextView; // a reference to the textview we will update.
+
+	//This is the handler - here we specify how to react to any messages.
+	static Handler handler = new Handler() {
+
+		  //this code will run in the UI thread
+		    @Override
 		  public void handleMessage(Message msg) {
+			  //get the data from the message
 		      Bundle bundle = msg.getData();
 			  String string = bundle.getString("myKey");
-			  
-			  TextView myTextView = 
-                    (TextView)findViewById(R.id.myTextView);
+
+			 //update our textview
 			  myTextView.setText(string);
 		     }
 		 };
@@ -30,6 +35,8 @@ public class ThreadExampleActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_thread_example);
+		//get our reference to the textview.
+		myTextView = (TextView)findViewById(R.id.myTextView);
 	}
 
 	@Override
@@ -41,22 +48,27 @@ public class ThreadExampleActivity extends Activity {
 	
     public void buttonClick(View view)
     {
-
+		//this code will run in a background thread.
     	Runnable runnable = new Runnable() {
 	        public void run() {     
-
+				//get a new message for us to use from the handler
             	Message msg = handler.obtainMessage();
     			Bundle bundle = new Bundle();
+				//put the date in our bundle
     			SimpleDateFormat dateformat = 
                     new SimpleDateFormat("HH:mm:ss MM/dd/yyyy");
     			String dateString = dateformat.format(new Date());
+				//put the date string into the bundle
     			bundle.putString("myKey", dateString);
-                 msg.setData(bundle);
-                 handler.sendMessage(msg);
+				msg.setData(bundle);
+				//send the message - this will be sent to the handler
+				handler.sendMessage(msg);
 	        }
         };
+		//we want a new thread
         Thread mythread = new Thread(runnable);
-        mythread.start();
+        //lets start the thread (when the button is clicked)
+		mythread.start();
 
     }
 
